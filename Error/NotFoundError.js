90bfd9f2531e379d340390d9b338b900e5fe0af0
arwegasira <87615127+arwegasira@users.noreply.github.com>
2023-03-17 07:43:29 +0200

    Update NotFoundError.js

--- Error/NotFoundError.js
+++ Error/NotFoundError.js
@@ -4,6 +4,7 @@ const {StatusCodes} = require('http-status-codes')
 class NotFoundError extends CustomError {
 
     constructor(message) {
+        super(message)
         this.statusCode = StatusCodes.NOT_FOUND
     }
 }
